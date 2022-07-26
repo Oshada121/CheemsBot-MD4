484ce269d89a8d489c52fffaba4ca75ccbd50e89
Oshada121 <110058163+Oshada121@users.noreply.github.com>
2022-07-27 01:28:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -55,22 +55,22 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.ownername = "𝐤𝐢𝐧𝐠.𝐧𝐚𝐰𝐢𝐲𝐚" //ur owner name
+global.ytname = "𝐧𝐚𝐚𝐢𝐲𝐚" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['94767383752']
+global.ownertag = '94767383752' //ur tag number
+global.botname = '𝐤𝐢𝐧𝐠 𝐧𝐚𝐰𝐢𝐲𝐚 𝐛𝐨𝐭💥🤡💫' //ur bot name
+global.linkz = "" //your theme url which will be displayed on whatsapp
+global.websitex = "" //ur website to be displayed
+global.botscript = '' //script link
 global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.themeemoji = "🤡" //ur theme emoji
+global.packname = "𝐤𝐢𝐧𝐠 𝐧𝐚𝐰𝐢𝐲𝐚💥" //ur sticker watermark packname
+global.author = "𝐤𝐢𝐧𝐠 𝐧𝐚𝐰𝐢𝐡𝐚🤡" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['94767383752'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -102,7 +102,7 @@ global.ntilink = []
 //omther
 global.sessionName = 'session'
 global.antitags = true
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!'🥵,'🥲.'🥰,'🤩','😋','😅']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
